a13fb28154eb7ea68c62845526c6759d79bea4f9
Vineet <vineetkumar12392@gmail.com>
2025-07-06 23:43:09 +0530

    Update GitHub Pages configuration with actual username and repo name

--- twitter-clone/vite.config.js
+++ twitter-clone/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPOSITORY_NAME/',
+  base: '/Social_Media/',
 })
